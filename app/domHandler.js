fa8207b01b12116fc16bc9e6bbd834508e2dd972
Juan Rigual <juan.rigual.contractor.gdevtools.com@bbva.com>
2020-04-18 14:06:27 -0300

    fixed handleDom validations

--- app/domHandler.js
+++ app/domHandler.js
@@ -1,25 +1,43 @@
- function showInDom(pokemon, GENERATION) {
-    let sprite = pokemon.sprites.front_default;
-    let title = pokemon.species.name;
-    let id = pokemon.id;
-    let firstType = pokemon.types[0].type.name;
-    let secondType = pokemon.types.length > 1 ? pokemon.types[1].type.name : '';
-    let div = document.createElement('div');
+ function handleShowInDom(pokemon, GENERATION) {
+    let div = createDiv();
     div.className = isAlolaGeneration(GENERATION);
-    div.innerHTML = `
-        <h4>${title}<h4>
-        <small class="id">#00${id}</small>
-        <img src='${sprite}'>
-        <div class="types">
-            <span class=${firstType.toLowerCase()}>${firstType}</span>
-            <span clasS=${secondType.toLowerCase()}>${secondType}</span>
-        </div>
-    `
+    div.innerHTML = createInnerHtml(pokemon);
     document.querySelector('#container-pokemon').appendChild(div);
     this.dispatchEvent(new CustomEvent('add-pokemon', {'detail': GENERATION.total}));
+}
+
+function createDiv() {
+    return document.createElement('div');
+}
+
+function createInnerHtml(pokemon) {
+    let firstType = getFirstType(pokemon);
+    let secondType = getSecondType(pokemon);
+    return `
+    <a href="#">
+    <h4>${pokemon.species.name}</h4>
+    <small class="id">#00${pokemon.id}</small>
+    <img src='${pokemon.sprites.front_default}'>
+    <div class="types" >
+        <span class=${firstType}>${firstType}</span>
+        <span clasS=${secondType}>${secondType}</span>
+    </div>
+    </a>
+    `
 }
 
 function isAlolaGeneration(GENERATION) {
     let pokemonClass = GENERATION.generation_number === 7 ? 'div-pokemon alola': 'div-pokemon';
     return pokemonClass;
+}
+
+function getFirstType(pokemon) {
+    return pokemon.types[0].type.name;
+}
+
+function getSecondType(pokemon) {
+    if(pokemon.types.length > 1) {
+        return pokemon.types[1].type.name;
+    }
+    return '';
 }
